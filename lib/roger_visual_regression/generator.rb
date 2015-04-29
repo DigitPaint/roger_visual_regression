cfa78a09b9f2d9518e6dc02ae9fb1a0ea46fb8d1
Danny Cobussen <danny@digitpaint.nl>
2015-04-29 14:45:27 +0200

    Also match *.erb files and made feedback neater

--- lib/roger_visual_regression/generator.rb
+++ lib/roger_visual_regression/generator.rb
@@ -14,21 +14,32 @@ module RogerVisualRegression
 
     desc "Creates base spec files for visual regression"
     class_option(
-      :path,
+      :spec_path,
       type: :string,
-      default: "/specs/",
+      default: "specs/",
       desc: "Path where spec files should be stored"
     )
 
+    class_option(
+      :html_path,
+      type: :string,
+      desc: "HTML Path where the html files are located"
+    )
+
     def call
+      if options[:html_path].nil?
+        options[:html_path] = @project.options[:html_path]
+      end
+
       check_existing_spec_files(options)
 
       # Delete current specs found in folder
       delete_current_specs!(options)
 
-      create_base_specs!(test)
+      create_base_specs!
 
-      puts "Created base specifications in /#{options[:spec_path]}"
+      say "\n\n"
+      say_status "OK", "Created base specifications in /#{options[:spec_path]}"
     end
 
     private
@@ -37,8 +48,8 @@ module RogerVisualRegression
       spec_files = Dir.glob("#{options[:spec_path]}/**")
       unless spec_files.empty?
         if !(prompt("Path '#{options[:spec_path]}' not empty, do you want to continue? [Y/n]: ") =~ /^y(es)?$/i)
-          puts "Stopped initializing spec folder"
-          exit(1)
+          say_status "OK", "Stopped initializing spec folder", :red
+          exit(0)
         end
       end
     end
@@ -47,30 +58,33 @@ module RogerVisualRegression
       FileUtils.rm_rf(Dir.glob("#{options[:spec_path]}/*"))
     end
 
-    def create_base_specs!(test)
-      html_files = Dir[File.join(test.project.options[:html_path], "/**/*.html")]
+    def create_base_specs!
+      html_files = Dir[File.join(@project.options[:html_path], "/**/*.html{.erb}")]
       html_files.each do |html_file|
         # Keep original path
         original_file_path = html_file.dup
         html_file.slice! /^html/i
 
         # Create folder structure
-        spec_file_path = FileUtils.mkpath("spec" + File.dirname(html_file)).first
+        spec_file_path = FileUtils.mkpath(options[:spec_path] + File.dirname(html_file)).first
 
         # Create file with content
+        html_file = File.expand_path(File.basename(html_file, File.extname(html_file)))
         spec_file = (spec_file_path + "/" + File.basename(html_file, ".html") + "_spec.rb")
-        FileUtils.touch(spec_file)
 
         # Add content to the files for base testing
+        FileUtils.touch(spec_file)
         open(spec_file, 'a') do |f|
           f << "describe \"Base test for #{original_file_path}\", :type => :feature, :js => true do\n"
           f << "  before(:each) do\n"
-          f << "    visit \"http://localhost:#{test.project.server.server_options[:Port]}#{html_file}\"\n"
+          f << "    visit \"http://localhost:#{@project.server.server_options[:Port]}#{html_file}\"\n"
           f << "    sleep 2\n"
           f << "  end\n"
           f << "  it { page.should match_expectation } \n"
           f << "end"
         end
+
+        say_status "OK", "Created spec for #{html_file}"
       end
     end
 
